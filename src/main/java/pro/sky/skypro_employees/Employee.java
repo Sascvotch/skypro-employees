3ddc65c65f2310a893f2712edf0223fe69cd3569
Sascvotch <sascvotch@yandex.ru>
2022-04-16 22:21:30 +0300

    Мапы

--- src/main/java/pro/sky/skypro_employees/Employee.java
+++ src/main/java/pro/sky/skypro_employees/Employee.java
@@ -1,31 +1,28 @@
 package pro.sky.skypro_employees;
-
 import java.util.Objects;
 
 public class Employee {
-
+    private final String key;
     private String firstName;
     private String lastName;
 
-    public Employee(String firstName, String lastName) {
+    public Employee( String firstName, String lastName) {
 
         this.firstName = firstName;
         this.lastName = lastName;
+        this.key= firstName + lastName;
     }
 
-
+    public String getKey() {return key;}
     public String getFirstName() {
         return firstName;
     }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
@@ -35,18 +32,20 @@ public class Employee {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Employee employee = (Employee) o;
-        return firstName.equals(employee.firstName) && lastName.equals(employee.lastName);
+        return key.equals(employee.key);
+
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(firstName, lastName);
+        return Objects.hash(key,firstName, lastName);
     }
 
     @Override
     public String toString() {
         return "Employee{" +
-                "name='" + firstName + '\'' +
+                ", key='" + key + '\'' +
+                ", name='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 '}';
     }
