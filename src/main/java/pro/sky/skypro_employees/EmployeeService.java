4e35a0b3c37327df8741062a8453ddf79ce88f2a
Sascvotch <sascvotch@yandex.ru>
2022-04-17 20:01:34 +0300

    Мапы

--- src/main/java/pro/sky/skypro_employees/EmployeeService.java
+++ src/main/java/pro/sky/skypro_employees/EmployeeService.java
@@ -5,8 +5,5 @@ import java.util.List;
 public interface EmployeeService {
     Employee employeeAdd (String firstName, String lastName);
     Employee employeeFind (String firstName, String lastName);
-    Employee employeeContains (String firstName, String lastName);
     Employee employeeRemove (String firstName, String lastName);
-    String employeeList();
-    List <Employee> employeeListEmp();
 }
