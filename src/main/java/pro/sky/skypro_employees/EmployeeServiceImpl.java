9c16dcf7615938bdd84c231f8fe2f1d4489b0a25
Sascvotch <sascvotch@yandex.ru>
2022-04-17 15:12:35 +0300

    Мастер Листы

--- src/main/java/pro/sky/skypro_employees/EmployeeServiceImpl.java
+++ src/main/java/pro/sky/skypro_employees/EmployeeServiceImpl.java
@@ -8,6 +8,7 @@ import java.util.List;
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
     List<Employee> employee = new ArrayList<>();
+
     public List<Employee> employeeListEmp() {
         return employee;
 
